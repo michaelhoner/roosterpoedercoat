823e20aa7eb2a9150d0dca286ef680099bf60858
Michael <43703002+michaelhoner@users.noreply.github.com>
2019-05-10 08:37:19 +0200

    Updates

---
+++ precache-manifest.5595e710222b719cdc5184625370888b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18a7b3b5fbee9ea6b306d894e8f7d6a3",
+    "url": "/roosterpoedercoat/index.html"
+  },
+  {
+    "revision": "319a1061a7eaa525b8ee",
+    "url": "/roosterpoedercoat/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/roosterpoedercoat/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "319a1061a7eaa525b8ee",
+    "url": "/roosterpoedercoat/static/js/main.a47f48ab.chunk.js"
+  },
+  {
+    "revision": "c88db055e7b12b9e1ff5",
+    "url": "/roosterpoedercoat/static/js/runtime~main.0da09e6b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/roosterpoedercoat/static/media/logo.5d5d9eef.svg"
+  }
+]);

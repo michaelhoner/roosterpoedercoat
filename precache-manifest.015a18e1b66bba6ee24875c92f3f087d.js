ec4f45289713dd1381da6ab86e11d5d5a7d78796
Michael <43703002+michaelhoner@users.noreply.github.com>
2019-05-14 13:03:09 +0200

    Updates

--- precache-manifest.015a18e1b66bba6ee24875c92f3f087d.js
+++ precache-manifest.015a18e1b66bba6ee24875c92f3f087d.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4bef365b3186e05505039722a4f7e2f5",
+    "revision": "47e2ddbe3a9089cf30d7ef62992339ef",
     "url": "/roosterpoedercoat/index.html"
   },
   {
-    "revision": "d177016d911d7de46c15",
+    "revision": "921c3e139f1a38d656ba",
     "url": "/roosterpoedercoat/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "d3e4e88ed6c8af69f846",
-    "url": "/roosterpoedercoat/static/js/2.1ebd355f.chunk.js"
+    "revision": "ebb59c25c179297b03d0",
+    "url": "/roosterpoedercoat/static/js/2.a599a94f.chunk.js"
   },
   {
-    "revision": "d177016d911d7de46c15",
-    "url": "/roosterpoedercoat/static/js/main.1940e930.chunk.js"
+    "revision": "921c3e139f1a38d656ba",
+    "url": "/roosterpoedercoat/static/js/main.0d852e05.chunk.js"
   },
   {
     "revision": "c88db055e7b12b9e1ff5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/roosterpoedercoat/precache-manifest.683c0bf318e333cc8b2aaa0bf0f18c20.js"
+  "/roosterpoedercoat/precache-manifest.015a18e1b66bba6ee24875c92f3f087d.js"
 );
 
 self.addEventListener('message', (event) => {
